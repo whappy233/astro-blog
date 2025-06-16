2051b365702e7d52389a7056a3bc063fd2f19f5e
Carlos <carlos.wu.h@foxmail.com>
2025-06-17 00:02:17 +0800

    init commit

---
+++ src/consts.ts
@@ -0,0 +1,62 @@
+import type { IconMap, SocialLink, Site } from '@/types'
+
+export const SITE: Site = {
+  title: 'Carlos Blog',
+  description: 'Bolg description',
+  href: 'https://astro-erudite.vercel.app',
+  author: 'Carlos',
+  locale: 'zh-CN',
+  featuredPostCount: 3,
+  postsPerPage: 10,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/blog',
+    label: 'Blog',
+  },
+  {
+    href: '/tags',
+    label: 'Tags',
+  },
+  {
+    href: '/notes',
+    label: 'Notes',
+  },
+  {
+    href: '/photos',
+    label: 'Photos',
+  },
+  {
+    href: '/about',
+    label: 'About',
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://github.com/whappy233',
+    label: 'GitHub',
+  },
+  // {
+  //   href: 'https://twitter.com/enscry',
+  //   label: 'Twitter',
+  // },
+  {
+    href: 'mailto:carlos.wu.h@foxmail.com',
+    label: 'Email',
+  },
+  {
+    href: '/rss.xml',
+    label: 'RSS',
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
---
+++ src/content/authors/enscribe.md
@@ -0,0 +1,10 @@
+---
+name: 'Carlos Wu'
+pronouns: 'he/him'
+avatar: 'https://gravatar.com/avatar/e7072ae191c2f85d8533871715bb5cfc?size=256'
+bio: '🙈'
+# website: ''
+# twitter: ''
+github: 'https://github.com/whappy233'
+mail: 'carlos.wu.h@foxmail.com'
+---
